4b1a946ad09c1ad6ca825b05c5633ddbd6d0fcbe
rtnxs <riotnexus@mail.ru>
2020-03-20 20:21:39 +0300

    added fontawesome, item style, edit item views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,4 @@ require("channels")
 
 import 'bootstrap'
 import '../stylesheets/application.scss'
+import '@fortawesome/fontawesome-free/js/all'
