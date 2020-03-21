af29dee0c02695e01fd3900e7cf9b4a7f9d066f3
rtnxs <riotnexus@mail.ru>
2020-03-21 20:30:15 +0300

    added gem ransack for search

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'simple_form'
 gem 'webdrivers', '~> 4.0'
 gem 'devise'
 gem 'devise-i18n'
+gem 'ransack', github: 'activerecord-hackery/ransack'
 # gem 'image_processing', '~> 1.2'
 
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,8 @@ class ItemsController < ApplicationController
   before_action :set_item, only: %i[show edit update destroy]
 
   def index
-    @items = Item.all
+    @q = Item.ransack(params[:q])
+    @items = @q.result(distinct: true)
   end
 
   def show
