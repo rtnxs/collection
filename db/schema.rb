265eab7700d81109e34cdd5de55babaadec74aec
rtnxs <riotnexus@mail.ru>
2020-07-20 03:19:57 +0300

    fix new item with curent user exist?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_172320) do
+ActiveRecord::Schema.define(version: 2020_03_09_171703) do
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
