6abc269c2f58e4f885a1065faf79aa41f1ba3ed7
rtnxs <riotnexus@mail.ru>
2020-07-23 22:26:56 +0300

    ref update profile form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :account_update,
-      keys: %i[password password_confirmation current_password]
+        :account_update,
+        keys: %i[role password password_confirmation current_password]
     )
 
     devise_parameter_sanitizer.permit(
-      :sign_up,
-      keys: %i[name password password_confirmation current_password]
+        :sign_up,
+        keys: %i[name password password_confirmation current_password]
     )
   end
 end
