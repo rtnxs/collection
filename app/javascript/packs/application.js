fcb898f0679fbc3e5658cd5aa2b1aefb4441303d
rtnxs <riotnexus@mail.ru>
2020-07-28 17:49:13 +0300

    lightbox2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,3 +20,6 @@ require("channels")
 import 'bootstrap'
 import '../stylesheets/application.scss'
 import '@fortawesome/fontawesome-free/js/all'
+
+// import 'lightbox2/src/js/lightbox'
+// import 'lightbox2/dist/js/lightbox-plus-jquery.min'
