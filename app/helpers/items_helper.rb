11867e115cd9798a096e0ca2c253368ab2d2b4cd
rtnxs <riotnexus@mail.ru>
2020-08-06 17:42:29 +0300

    added item background img

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -6,4 +6,12 @@ module ItemsHelper
   def direct_cont(index)
     index.odd? ? 'flex-row' : 'flex-row-reverse'
   end
+
+  def random_coin_background
+    %w[assets/coin.jpg assets/coin2.jpg assets/coin3.jpg].sample
+  end
+
+  def random_mark_background
+    %w[assets/mark.jpg assets/mark2.jpg].sample
+  end
 end
